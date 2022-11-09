cd5360eb9e0138e959655d8833438b5d4eef25a4
ahnaf4749 <ahnafsagor12@gmail.com>
2022-11-09 23:15:45 +0600

    review added

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z9zwita.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
 
     try {
         const seviceCullection = client.db('woodWorker').collection('services');
+        const reviewCollection = client.db('woodWorker').collection('reviews')
 
         app.get('/limitservices', async (req, res) => {
             const query = {}
@@ -37,6 +37,14 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const service = await seviceCullection.findOne(query);
             res.send(service);
+        })
+
+        // review details
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
         })
     }
     finally {
